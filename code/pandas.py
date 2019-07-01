a594f63e5745623f0e51181e5c634114c31e64f1
sumant singh <48150206+sumant039@users.noreply.github.com>
2019-07-01 22:33:25 +0530

    check

---
+++ code/pandas.py
@@ -0,0 +1,311 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+x=np.array([10,20,30])
+print(x)
+
+
+# In[3]:
+
+
+x=np.array([1,2,3.0])
+print(x)
+
+
+# In[5]:
+
+
+type(x)
+
+
+# In[10]:
+
+
+x=np.array([1,2,3],ndmin=3)
+
+
+# In[11]:
+
+
+print(x)
+
+
+# In[13]:
+
+
+x=np.array([1,2,3],dtype=float)
+print(x)
+
+
+# In[14]:
+
+
+x=np.array([1,2,3],dtype=float)
+print(x)
+
+
+# In[16]:
+
+
+x=np.array([[1,2],[3,4]])
+print(x)
+
+
+# In[17]:
+
+
+x=np.array([1,2,3,4])
+print(x)
+
+
+# In[19]:
+
+
+x=np.array([(1,2,3),(4,5,6)])
+print(x)
+
+
+# In[21]:
+
+
+x=np.array([(1,2,3),(4,5,6)])
+print(x)
+
+
+# In[22]:
+
+
+type(x)
+
+
+# In[24]:
+
+
+x=np.zeros((3,2))
+print(x)
+
+
+# In[27]:
+
+
+x=np.ones(100)
+print(x)
+
+
+# In[33]:
+
+
+x=np.ones(10)
+print(x)
+
+
+# In[34]:
+
+
+x=np.ones((2,3,4))
+print(x)
+
+
+# In[35]:
+
+
+x=np.ones((2,3,4))
+print(x)
+
+
+# In[37]:
+
+
+x=np.empty((2,3))
+print(x)
+
+
+# In[38]:
+
+
+x=np.array([[10,20,30],[40,50,60]])
+print(x)
+
+
+# In[39]:
+
+
+x=np.array([[10,20,30],[40,50,60]])
+print(x)
+
+
+# In[40]:
+
+
+import numpy as np
+x=np.arange(3)
+print(x)
+
+
+# In[42]:
+
+
+import numpy as np
+x=np.arange(10)
+print(x)
+
+
+# In[43]:
+
+
+x=np.arange(3.0)
+
+
+# In[44]:
+
+
+print(x)
+
+
+# In[45]:
+
+
+x=np.arange(1,10)
+print(x)
+
+
+# In[46]:
+
+
+x=np.arange(1,10,2)
+print(x)
+
+
+# In[47]:
+
+
+x=np.array([123,123,123])
+print(x)
+
+
+# In[49]:
+
+
+print(type(x))
+
+
+# In[50]:
+
+
+x=np.arange(1,10)
+print(x)
+
+
+# In[51]:
+
+
+type(x)
+
+
+# In[53]:
+
+
+x=np.arange(10)
+print(type(x))
+
+
+# In[54]:
+
+
+x=np.arange(1,50,3)
+print(x)
+
+
+# In[55]:
+
+
+x=np.arange(1,5,.1)
+print(x)
+
+
+# In[56]:
+
+
+x=np.arange(1,5,.2)
+print(x)
+
+
+# In[61]:
+
+
+x=np.linspace(1,2,100)
+print(x)
+
+
+# In[65]:
+
+
+x=np.linspace(1,2,5)
+print(type(x))
+
+
+# In[64]:
+
+
+type(x)
+
+
+# In[66]:
+
+
+x=np.linspace(2.0,3.0,num=5)
+print(x)
+
+
+# In[68]:
+
+
+x=np.linspace(2.0,3.0,5,endpoint=False)
+print(x)
+
+
+# In[69]:
+
+
+x=np.zeros(8)
+print(x)
+x=np.linspace(0,10,8)
+print(x)
+
+
+# In[72]:
+
+
+import numpy as np
+x=np.array([6])
+print(x)
+
+
+# In[73]:
+
+
+print(type(x))
+
+
+# In[76]:
+
+
+x=np.arange(6)
+type(x)
+
+
+# In[77]:
+
+
+print(x)
+
+
+# In[ ]:
+
+
+x=np.arange(12).reshape()
+
